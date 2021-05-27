dc95cafe0927afefb85ced0edc80ae179f7d6c8c
mvcbohyun <jbh959847@gmail.com>
2021-05-27 11:37:52 +0900

    회원 조회 , 수정 api 추가

--- src/main/java/jpabook/jpabook/api/MemberApiController.java
+++ src/main/java/jpabook/jpabook/api/MemberApiController.java
@@ -1,14 +1,21 @@
 package jpabook.jpabook.api;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import jpabook.jpabook.domain.Member;
 import jpabook.jpabook.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
@@ -17,6 +24,49 @@ import lombok.RequiredArgsConstructor;
 public class MemberApiController {
 
 	private final MemberService memberService;
+	 /**
+	 * 조회 V1: 응답 값으로 엔티티를 직접 외부에 노출한다.
+	 * 문제점
+	 * - 엔티티에 프레젠테이션 계층을 위한 로직이 추가된다.
+	 * - 기본적으로 엔티티의 모든 값이 노출된다.
+	 * - 응답 스펙을 맞추기 위해 로직이 추가된다. (@JsonIgnore, 별도의 뷰 로직 등등)
+	 * - 실무에서는 같은 엔티티에 대해 API가 용도에 따라 다양하게 만들어지는데, 한 엔티티에 각각의
+	API를 위한 프레젠테이션 응답 로직을 담기는 어렵다.
+	 * - 엔티티가 변경되면 API 스펙이 변한다.
+	 * - 추가로 컬렉션을 직접 반환하면 항후 API 스펙을 변경하기 어렵다.(별도의 Result 클래스
+	생성으로 해결)
+	 * 결론
+	 * - API 응답 스펙에 맞추어 별도의 DTO를 반환한다.
+	 */
+	 //조회 V1: 안 좋은 버전, 모든 엔티티가 노출, @JsonIgnore -> 이건 정말 최악, api가 이거
+	//하나인가! 화면에 종속적이지 마라!
+	@GetMapping("/api/v1/members")
+	public List<Member> memberV1(){
+		return memberService.findmebers();
+	}
+	
+	@GetMapping("/api/v2/members")
+	public Result memberV2(){
+		List<Member> findmembers=  memberService.findmebers();
+	
+		List<MemberDto> collect =findmembers.stream().map(m -> new MemberDto(m.getName())).collect(Collectors.toList());
+	
+		return new Result(collect.size(),collect);
+	}
+	
+	@Data
+	@AllArgsConstructor
+	static class Result<T>{
+		private int count;
+		private T data;
+	}
+	@Data
+	@AllArgsConstructor
+	static class MemberDto{
+		private String name ;
+	}
+
+
 	
 	@PostMapping("/api/v1/members")
 	public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
@@ -34,6 +84,28 @@ public class MemberApiController {
 	return new CreateMemberResponse(id);
 	}
 	
+	@PutMapping("/api/v2/members/{id}")
+	public UpdateMemberResponse updateMemberV2(@PathVariable("id")Long id 
+											, @RequestBody @Valid UpdateMemberRequest request) {
+		System.out.println("111111111");
+		memberService.update(id,request.name);
+		Member findMemeber =memberService.findOne(id);
+		return new UpdateMemberResponse(findMemeber.getId(), findMemeber.getName());
+	}
+	
+	@Data
+	static class UpdateMemberRequest{
+		
+		private String name;
+		
+	}
+	@Data
+	@AllArgsConstructor
+	static class UpdateMemberResponse{
+		private Long id ;
+		private String name;
+		
+	}
 	
 	@Data
 	static class CreateMemberRequest{
