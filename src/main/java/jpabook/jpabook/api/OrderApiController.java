7371e7b14e4ea1d878bd9f51f88253c1f175a11e
mvcbohyun <jbh959847@gmail.com>
2021-06-01 12:03:06 +0900

    ToMany Dto 조회 추가

--- src/main/java/jpabook/jpabook/api/OrderApiController.java
+++ src/main/java/jpabook/jpabook/api/OrderApiController.java
@@ -1,14 +1,19 @@
 package jpabook.jpabook.api;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jpabook.jpabook.domain.Address;
 import jpabook.jpabook.domain.Order;
 import jpabook.jpabook.domain.OrderItem;
+import jpabook.jpabook.domain.OrderStatus;
 import jpabook.jpabook.repository.OrderRepository;
 import jpabook.jpabook.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 /**
@@ -34,22 +39,61 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 public class OrderApiController {
-	
+
 	private final OrderRepository orderRepository;
-	 /**
+	/**
 	 * V1. 엔티티 직접 노출
 	 * - Hibernate5Module 모듈 등록, LAZY=null 처리
 	 * - 양방향 관계 문제 발생 -> @JsonIgnore
 	 */
-	 @GetMapping("/api/v1/orders")
-	 public List<Order> ordersV1() {
-	 List<Order> all = orderRepository.findAll( new OrderSearch());
-	 for (Order order : all) {
-	 order.getMember().getName(); //Lazy 강제 초기화
-	 order.getDelivery().getAddress(); //Lazy 강제 초기환
-	 List<OrderItem> orderItems = order.getOrderItems();
-	 orderItems.stream().forEach(o -> o.getItem().getName()); //Lazy 강제초기화
+	@GetMapping("/api/v1/orders")
+	public List<Order> ordersV1() {
+		List<Order> all = orderRepository.findAll( new OrderSearch());
+		for (Order order : all) {
+			order.getMember().getName(); //Lazy 강제 초기화
+			order.getDelivery().getAddress(); //Lazy 강제 초기환
+			List<OrderItem> orderItems = order.getOrderItems();
+			orderItems.stream().forEach(o -> o.getItem().getName()); //Lazy 강제초기화
+		}
+		return all;
+	}
+
+	@GetMapping("/api/v2/orders")
+	public List<OrderDto> ordersV2() {
+		List<Order> orders = orderRepository.findAll(new OrderSearch());
+		List<OrderDto> result = orders.stream()
+				.map(o -> new OrderDto(o))
+				.collect(Collectors.toList());
+		return result;
+	}
+	@Data
+	static class OrderDto {
+		private Long orderId;
+		private String name;
+		private LocalDateTime orderDate; //주문시간
+		private OrderStatus orderStatus;
+		private Address address;	
+		private List<OrderItemDto> orderItems;
+		public OrderDto(Order order) {
+			orderId = order.getId();	
+			name = order.getMember().getName();
+			orderDate = order.getOrderDate();
+			orderStatus = order.getStatus();
+			address = order.getDelivery().getAddress();
+			orderItems = order.getOrderItems().stream()
+					.map(orderItem -> new OrderItemDto(orderItem))
+					.collect(Collectors.toList());
+		}
+	}
+	@Data
+	static class OrderItemDto {
+	 private String itemName;//상품 명
+	 private int orderPrice; //주문 가격
+	 private int count; //주문 수량
+	 public OrderItemDto(OrderItem orderItem) {
+	 itemName = orderItem.getItem().getName();
+	 orderPrice = orderItem.getOrderPrice();
+	 count = orderItem.getCount();
 	 }
-	 return all;
-	 }
+	}
 }
